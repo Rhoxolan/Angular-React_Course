f4a1634d4582294caca3e4821f7972126eeb8200
Rhoxolan <pba17mail@gmail.com>
2023-06-13 21:22:48 +0300

    Updated the sixth practical work project and added bootstrap ino this project

--- 2023.03.31_PW/Hobby/src/app/app.module.ts
+++ 2023.03.31_PW/Hobby/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { MyHobbyComponent } from './my-hobby/my-hobby.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
