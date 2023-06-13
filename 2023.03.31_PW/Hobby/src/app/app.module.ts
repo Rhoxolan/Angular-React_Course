b3683be32ddf06099160c57b41fbe5d9b17378a3
Rhoxolan <pba17mail@gmail.com>
2023-06-13 21:15:10 +0300

    The work on the sixth practical is ongoing

--- 2023.03.31_PW/Hobby/src/app/app.module.ts
+++ 2023.03.31_PW/Hobby/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { MyHobbyComponent } from './my-hobby/my-hobby.component';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
