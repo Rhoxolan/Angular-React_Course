fafd7ecb393c486fc85902d1af69a30916c794fd
Rhoxolan <pba17mail@gmail.com>
2023-06-19 23:21:43 +0300

    The eight practical work was supplemented

--- 2023.06.04_PW/Server/app.js
+++ 2023.06.04_PW/Server/app.js
@@ -3,10 +3,20 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/paintings', (req, res) => {
     //fs.createReadStream('./paintings.json').pipe(res);
     res.sendFile(__dirname + "/paintings.json");
 });
+
+app.get('/paintings/:id', (req, res) => {
+    let id = req.params.id;
+    fs.readFile("db.json", function(err, data) {
+        let dbData = JSON.parse(data);
+        console.log(data);
+    });
+    res.status(200).send();
+});
+
 
 
 app.listen(port, () => {
