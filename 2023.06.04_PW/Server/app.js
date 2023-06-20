e7f62191214a31a1188eff6c9591de4352ec1ffe
Rhoxolan <pba17mail@gmail.com>
2023-06-20 11:02:34 +0300

    The eight practical work was supplemented

--- 2023.06.04_PW/Server/app.js
+++ 2023.06.04_PW/Server/app.js
@@ -9,8 +9,30 @@ app.get('/paintings', (req, res) => {
 });
 
 app.get('/paintings/:id', (req, res) => {
+    fs.readFile("db.json", 'utf-8', function (err, data) {
+        if (err) {
+            console.error(err);
+            //return res.status(500).send("1Internal Server Error");
+        }
+        try {
+            let id = req.params.id;
+            let dbData = JSON.parse(data);
+            let painting = dbData.find(item => item.id == id);
+            if (!painting) {
+                //return res.status(404).send("2Painting not found");
+            }
+            return res.sendFile(__dirname + painting.path);
+        }
+        catch {
+            //return res.status(500).send("3Internal Server Error");
+        }
+    });
+    //return res.status(500).send("4Internal Server Error");
+});
+
+app.get('/paintings/:id/size/100', (req, res) => {
     let id = req.params.id;
-    fs.readFile("db.json", function(err, data) {
+    fs.readFile("db.json", 'utf-8', function (err, data) {
         let dbData = JSON.parse(data);
         console.log(data);
     });
