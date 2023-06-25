d662c7a9f6ff646d58174474597bacb3d2547149
Rhoxolan <pba17mail@gmail.com>
2023-06-26 00:09:17 +0300

    The work on the nineth practical work is ongoing

--- 2023.04.11_PW/OpenMovie/src/app/app-routing.module.ts
+++ 2023.04.11_PW/OpenMovie/src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchFilmComponent } from './search-film/search-film.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
   { path: "", component: SearchFilmComponent, title: "Search Film" },
-  { path: "about", component: AboutComponent, title: "About" },
+  { path: "search", component: SearchFilmComponent, title: "Search Film" },
+  {
+    path: "about", component: AboutComponent, title: "About", children: [
+      { path: "company", component: AboutCompanyComponent, title: "About Company" },
+      { path: "contacts", component: ContactsComponent, title: "Contacts" }
+    ]
+  },
   { path: "**", component: NotFoundComponent, title: "Not found!" }
 ];
 
--- 2023.04.11_PW/OpenMovie/src/app/app.module.ts
+++ 2023.04.11_PW/OpenMovie/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { SearchFilmComponent } from './search-film/search-film.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFilmComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AboutCompanyComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
