dce177f84104f68497419f76f271c393e8a8c4ad
Rhoxolan <pba17mail@gmail.com>
2023-06-29 22:28:51 +0300

    The tenth practical work was supplemented

--- 2023.04.13_PW/ClientApp/UsersAccounting/src/app/app.module.ts
+++ 2023.04.13_PW/ClientApp/UsersAccounting/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    CreateComponent,
+    EditComponent,
+    DeleteConfirmComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
