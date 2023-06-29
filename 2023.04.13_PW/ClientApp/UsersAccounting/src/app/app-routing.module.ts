c4c5347d6c4035982b7919abe74e527773fddaed
Rhoxolan <pba17mail@gmail.com>
2023-06-29 23:44:10 +0300

    The tenth practical work was supplemented

--- 2023.04.13_PW/ClientApp/UsersAccounting/src/app/app-routing.module.ts
+++ 2023.04.13_PW/ClientApp/UsersAccounting/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: UserListComponent, title: "User's List" },
+  { path: "create", component: CreateComponent, title: "Create User" },
+  { path: "edit/:id", component: EditComponent, title: "Edit User" },
+  { path: "delete/:id", component: DeleteConfirmComponent, title: "Delete" },
+  { path: "**", component: NotFoundComponent, title: "Not Found!" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
