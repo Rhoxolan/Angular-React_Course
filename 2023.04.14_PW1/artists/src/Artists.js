abad79deee9011c3c34b994dc7fecf74748f82a1
Rhoxolan <pba17mail@gmail.com>
2023-07-06 18:42:06 +0300

    The first part of the  eleventh practical work was completed

---
+++ 2023.04.14_PW1/artists/src/Artists.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default class Artists extends React.Component {
+    render() {
+        return <div>
+        <h3>The TOP of my favourite artists!</h3>
+        <ul>
+            <li>
+                Lacrimosa (Gothic metal/Symphonic metal)
+            </li>
+            <li>
+                Metallica (Thrash metal/Heavy metal)
+            </li>
+            <li>
+                The Doors (Psychedelic rock/Blues rock)
+            </li>
+            <li>
+                John Du Cann (The Attack, Andromeda, Atomic Rooster) (Psychedelic rock/Blues rock/Hard rock)
+            </li>
+            <li>
+                The Sonics (Garage Rock)
+            </li>
+            <li>
+                Alice in Chains (Grunge)
+            </li>
+            <li>
+                The Beatles (Classic rock/Pop rock)
+            </li>
+            <li>
+                Led Zeppelin (Hard rock/Blues rock)
+            </li>
+            <li>
+                The Rasmus (Alternative rock)
+            </li>
+            <li>
+                Hollywood Undead (Rapcore)
+            </li>
+        </ul>
+      </div>;
+    }
+}
